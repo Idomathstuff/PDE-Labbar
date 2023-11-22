13fa10df299e8e7c5ea79174e63ec99d77987d06
Lucas <l.m.frykman@gmail.com>
2023-11-22 16:56:46 +0100

    reorganizing files

---
+++ lab1/uppgift2/characteristic_path.m
@@ -0,0 +1,15 @@
+K = 10; % K val (see lab1 description)
+
+uppgift2; 
+
+
+
+
+ds = 0.1; % time step size for time variable s (see lab1 desc.)
+time_stop = 10; % stop at 10 units of s  
+n_stop = round()
+
+x0 = 0;
+
+for i = 1:10
+end
